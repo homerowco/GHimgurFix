91078f9a0235d65e19264d9aab732a4e6f8d5edb
homerow-co <admin@homerow.co>
2018-06-27 01:11:52 +0200

    added a href query to make zoom in work as well

--- ext/chrome/src/craycopy/inject.js
+++ ext/chrome/src/craycopy/inject.js
@@ -1,7 +1,12 @@
 (function() {
-	for( e of document.querySelectorAll( 'img[src*="//i.imgur"]' ) ) {
+	for( e of document.querySelectorAll( 'img[src*="//i.imgur.com"]' ) ) {
 		if( e.src.indexOf('craycopy') === -1 ) {
 			e.src = 'https://craycopy.com/i/'+e.src;
+		}
+	}
+	for( e of document.querySelectorAll( 'a[href*="//i.imgur.com"]' ) ) {
+		if( e.href.indexOf('craycopy') === -1 ) {
+			e.href = 'https://craycopy.com/i/'+e.href;
 		}
 	}
 })();
