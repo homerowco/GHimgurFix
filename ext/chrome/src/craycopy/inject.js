907b88e085e42ff518e4fdbacd0557c77696c01e
trizkut <trizkut@gmail.com>
2018-06-26 18:56:22 -0400

    Fixed links to restore highslide thumbnail viewer behavior

--- ext/chrome/src/craycopy/inject.js
+++ ext/chrome/src/craycopy/inject.js
@@ -4,4 +4,12 @@
 			e.src = 'https://craycopy.com/i/'+e.src;
 		}
 	}
+
+	// link fix for highslide thumbnail viewer functionality
+	for( e of document.querySelectorAll( 'a[href*="//i.imgur"]' ) ) {
+		if( e.href.indexOf('craycopy') === -1 ) {
+			e.href = 'https://craycopy.com/i/'+e.href;
+		}
+	}
+
 })();
