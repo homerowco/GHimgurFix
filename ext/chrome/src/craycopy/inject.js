16e8939b87e888efe4b1f38041c80b073eedabb7
homerow-co <admin@homerow.co>
2018-06-27 02:44:46 +0200

    simplified injection loop

--- ext/chrome/src/craycopy/inject.js
+++ ext/chrome/src/craycopy/inject.js
@@ -1,14 +1,12 @@
 (function() {
-	var f = '://i.imgur.com/',
-		r = 'https://craycopy.com/';
-	for( e of document.querySelectorAll( 'img.bbc_img[src*="'+f+'"]' ) ) {
-		if( e.src.indexOf(r) === -1 ) {
-			e.src = r+'i/'+e.src;
-		}
-	}
-	for( e of document.querySelectorAll( 'a.highslide[href*="'+f+'"]' ) ) {
-		if( e.href.indexOf(r) === -1 ) {
-			e.href = r+'i/'+e.href;
+	var K = '://i.imgur.com/',
+		e = 'https://craycopy.com/',
+		E = [['img.bbc_img[','src'],['a.highslide[','href']],
+		G,H;
+	for( G in E ) {
+		for( H of document.querySelectorAll( E[G][0]+E[G][1]+'*="'+K+'"]' ) ) {
+			var x = H[E[G][1]];
+			-1 === x.indexOf(e) && ( H[E[G][1]] = e+'i/'+x );
 		}
 	}
 })();
