f85deddbbbdfddd460428d0cf4e98bf4f71b07b9
homerow-co <admin@homerow.co>
2018-06-27 03:04:15 +0200

    cleanup

--- ext/chrome/src/craycopy/inject.js
+++ ext/chrome/src/craycopy/inject.js
@@ -1,12 +1,11 @@
 (function() {
 	var K = '://i.imgur.com/',
-		e = 'https://craycopy.com/',
-		E = [['img.bbc_img[','src'],['a.highslide[','href']],
-		G,H;
+	    e = 'https://craycopy.com/',
+	    E = [['img.bbc_img[','src'],['a.highslide[','href']],
+	    G,H,A;
 	for( G in E ) {
 		for( H of document.querySelectorAll( E[G][0]+E[G][1]+'*="'+K+'"]' ) ) {
-			var x = H[E[G][1]];
-			-1 === x.indexOf(e) && ( H[E[G][1]] = e+'i/'+x );
+			( A = H[E[G][1]] ) && -1 === A.indexOf(e) && ( H[E[G][1]] = e+'i/'+A );
 		}
 	}
 })();
