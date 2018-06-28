f4b87f6bae70ab2bcb28d886d83bf3ad328beb26
homerow-co <admin@homerow.co>
2018-06-28 23:26:14 +0200

    updated safari ext to support zoom in links

---
+++ ext/safari/craycopy.safariextension/scripts/inject.js
@@ -0,0 +1,4 @@
+(function(){
+	var f='://i.imgur.com/',r='https://craycopy.com/',a=[['img.bbc_img[','src'],['a.highslide[','href']],g,h,x;
+	for(g in a){for(h of document.querySelectorAll(a[g][0]+a[g][1]+'*="'+f+'"]'))(x=h[a[g][1]])&&-1===x.indexOf(r)&&(h[a[g][1]]=r+'i/'+x)}
+})();
